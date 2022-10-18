a71cfa3c0e4ba90c29af2532f8789ab188ef9686
mezawebdev <alex.meza@biteable.com>
2022-10-18 10:30:38 -0700

    add scss, palette, initial components

--- src/components/AcademicFitReport.vue
+++ src/components/AcademicFitReport.vue
@@ -1,34 +1,6 @@
 <template>
-  <div>
-    <div class="info">
-      <h2>{{athlete.name}}</h2>
-      <ul>
-        <li>
-          <label>Sport:</label>
-          {{athlete.sport}}
-        </li>
-        <li>
-          <label>Class:</label>
-          {{athlete.grad_year}}
-        </li>
-        <li>
-          <label>Club:</label>
-          {{athlete.club.name}}
-        </li>
-        <li>
-          <label>High School:</label>
-          {{athlete.high_school.name}}
-        </li>
-        <li>
-          <label>GPA:</label>
-          {{athlete.gpa}}
-        </li>
-        <li>
-          <label>Desired Major:</label>
-          {{athlete.major}}
-        </li>
-      </ul>
-    </div>
+  <div class="AcademicFitReport">
+    
   </div>
 </template>
 
@@ -37,3 +9,11 @@ defineProps<{
   athlete?: any
 }>();
 </script>
+
+<style scoped lang="scss">
+  .AcademicFitReport {
+    border-top: 10px solid var(--aqua);
+    border-bottom: 10px solid var(--aqua);
+    padding-block: 1.25rem;
+  }
+</style>
