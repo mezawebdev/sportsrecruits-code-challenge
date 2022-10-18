0a3cdeb508bca6e04abb6cafac42cdbb8b7299e9
mezawebdev <alex.meza@biteable.com>
2022-10-18 01:49:25 -0700

    initial framework

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import AcademicFitReport from "@/components/AcademicFitReport.vue";
+import { data } from "./assets/data.json";
+</script>
+
+<template>
+  <main>
+    <AcademicFitReport :athlete="data[0]" />
+  </main>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/AcademicFitReport.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <div class="info">
+      <h2>{{athlete.name}}</h2>
+      <ul>
+        <li>
+          <label>Sport:</label>
+          {{athlete.sport}}
+        </li>
+        <li>
+          <label>Class:</label>
+          {{athlete.grad_year}}
+        </li>
+        <li>
+          <label>Club:</label>
+          {{athlete.club.name}}
+        </li>
+        <li>
+          <label>High School:</label>
+          {{athlete.high_school.name}}
+        </li>
+        <li>
+          <label>GPA:</label>
+          {{athlete.gpa}}
+        </li>
+        <li>
+          <label>Desired Major:</label>
+          {{athlete.major}}
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  athlete?: any
+}>();
+</script>
